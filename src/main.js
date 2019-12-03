b0a786d18417cbd7eadf745782ee02338724b644
Гендольф <ecehuh67@gmail.com>
2019-12-03 17:39:36 +0300

    added button loadMOre

--- src/main.js
+++ src/main.js
@@ -4,15 +4,14 @@ import {generateFilters} from './mocks/filter.js';
 import {generateTasks} from './mocks/task.js';
 import {createSortTemplate} from './components/sort.js';
 import {createBoardContainerTemplate} from './components/board.js';
-
-
-
-// import {createTaskTemplate} from './components/task.js';
-// import {createTaskEditTemplate} from './components/task-edit.js';
-// import {createLoadButtonTemplate} from './components/load-button.js';
-// import {createListOfTasksTemplate} from './components/task-list.js';
+import {createListOfTasksTemplate} from './components/task-list.js';
+import {createTaskTemplate} from './components/task.js';
+import {createTaskEditTemplate} from './components/task-edit.js';
+import {createLoadButtonTemplate} from './components/load-button.js';
 
 const TASK_COUNT = 20; // amount of card which needs rendering
+const TASK_COUNT_ON_START = 9;
+const TASK_COUNT_BY_BUTTON = 8;
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -30,22 +29,27 @@ render(mainElement, createFilterTemplate(filters), `beforeend`);
 render(mainElement, createBoardContainerTemplate(), `beforeend`);
 const boardContainerElement = mainElement.querySelector(`.board, container`);
 render(boardContainerElement, createSortTemplate(), `beforeend`);
+render(boardContainerElement, createListOfTasksTemplate(), `beforeend`);
+const boardTasksElement = boardContainerElement.querySelector(`.board__tasks`);
 
+render(boardTasksElement, createTaskEditTemplate(tasks[0]), `beforeend`);
+let showingTasksCount = TASK_COUNT_ON_START;
+tasks.slice(1, showingTasksCount).forEach((task) => render(boardTasksElement, createTaskTemplate(task), `beforeend`));
+render(boardContainerElement, createLoadButtonTemplate(), `beforeend`);
 
+const loadMoreButton = mainElement.querySelector(`.load-more`);
+loadMoreButton.addEventListener(`click`, () => {
+  const prevTaskCount = showingTasksCount;
+  showingTasksCount = showingTasksCount + TASK_COUNT_BY_BUTTON;
 
+  console.log(showingTasksCount);
+  console.log(tasks.length);
 
-// render(boardContainerElement, createFilterTemplate(), `beforeend`);
-// render(boardContainerElement, createListOfTasksTemplate(), `beforeend`);
-//
-// const boardTasksElement = boardContainerElement.querySelector(`.board__tasks`);
-// render(boardTasksElement, createEditFormTemplate(), `beforeend`);
-//
-// new Array(TASK_COUNT)
-//   .fill(``)
-//   .forEach(
-//       () => {
-//         render(boardTasksElement, createTaskTemplate(), `beforeend`);
-//       }
-//   );
-//
-// render(boardContainerElement, createLoadButtonTemplate(), `beforeend`);
+  tasks.slice(prevTaskCount, showingTasksCount).forEach((task) => {
+    return render(boardTasksElement, createTaskTemplate(task), `beforeend`);
+  });
+
+  if (showingTasksCount >= tasks.length) {
+    loadMoreButton.remove();
+  }
+});
--- src/mocks/util.js
+++ src/mocks/util.js
@@ -3,7 +3,7 @@ const castTimeFormat = (value) => {
 };
 
 export const formatTime = (date) => {
-  const hours = castTimeFormat(date.getHours % 12);
+  const hours = castTimeFormat(date.getHours() % 12);
   const minutes = castTimeFormat(date.getMinutes());
   const interval = date.getHours() > 11 ? `pm` : `am`;
 
